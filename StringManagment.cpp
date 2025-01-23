2781e6bca3882b5dede1cd1e9c09b867bb580571
David-Brnb <321171197@ciencias.unam.mx>
2025-01-22 22:18:32 -0600

    add: this dont work lol

--- StringManagment.cpp
+++ StringManagment.cpp
@@ -6,15 +6,25 @@ using namespace std;
 #ifndef STRINGMANAGMENT
 #define STRINGMANAGMENT
 
+/*
+    Esta clase se encarga de realizar el procesamiento de entradas, 
+    tales como definir el alfabeto, agregar operadores implícitos y 
+    cambiar la notación de infija a posfija en una expresión regular.
+*/
+
 class StringManagment {
 private: 
-    map<char, int> mapAbcNum;
-    set<char> abc;
+    set<char> abc; // Conjunto del alfabeto
 
+    /*
+        Método privado para refactorizar la entrada de la expresión regular. 
+        Agrega operadores definidos para la concatenación implícita.
+        Complejidad temporal: O(n), ya que recorre linealmente la cadena.
+    */
     string refactoriza(string &str){
         string news="";
         for(int i=0; i<str.size(); i++){
-            if(i!=0 && (str[i-1]==')' || str[i-1]=='*' || str[i-1]=='+' || mapAbcNum[str[i-1]] == 1) && (str[i]=='(' || mapAbcNum[str[i]] == 1)){
+            if(i!=0 && (str[i-1]==')' || str[i-1]=='*' || str[i-1]=='+' || abc.count(str[i])>0) && (str[i]=='(' || abc.count(str[i])>0)){
                 news+=".";
             }
             news+=str[i];
@@ -24,10 +34,15 @@ private:
     }
 
 public: 
+    // Constructor de la clase
     StringManagment(){};
 
+    /*  
+        Realiza el mapeo del alfabeto, verificando que no contenga caracteres reservados.
+        Complejidad temporal: O(n), donde n es el tamaño del alfabeto ingresado.
+        Retorna true si el mapeo es exitoso, false si contiene caracteres inválidos.
+    */
     bool mapeo(string str){
-        mapAbcNum.clear();
         abc.clear();
 
         for(auto e: str){
@@ -36,13 +51,21 @@ public:
         }
 
         for(auto e: str){
-            mapAbcNum[e]=1;
             abc.insert(e);
         }
 
         return true;
     }
 
+    /*
+        Convierte una expresión regular de notación infija a notación posfija.
+        Primero, refactoriza la expresión para agregar operadores implícitos.
+        Utiliza el algoritmo Shunting Yard propuesto por Edsger Dijkstra.
+        Complejidad temporal: O(n), donde n es el tamaño de la expresión regular, 
+        esto gracias a que las operaciones realizadas durante el procesamiento
+        tienen una complejidad de O(1) y se realizan de forma limitada por cada
+        carácter.
+    */
     string polish(string str){
         str = refactoriza(str);
         map<char, int> mapi;
@@ -58,7 +81,7 @@ public:
 
         for(int i=0; i<str.size(); i++){
             if(abc.count(str[i])>0){
-                q.push(str[i]);
+                q.push(str[i]); // Carácter del alfabeto
 
             } else if(str[i] == ')'){
                 while(!sti.empty() && sti.top() !='('){
@@ -67,7 +90,7 @@ public:
                 }
 
                 if(!sti.empty())
-                    sti.pop();
+                    sti.pop(); // Eliminar '('
 
             } else {
                 while(!sti.empty() && mapi[sti.top()]>=mapi[str[i]] && str[i]!='('){
@@ -92,6 +115,7 @@ public:
         return news;
     }
 
+    // Retorna el conjunto de caracteres del alfabeto
     set<char> getAlph(){
         return this->abc;
     }
--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@
 
 using namespace std; 
 
+/*
+    Clase principal que une las funcionalidades de las demás clases.
+    Se encarga de manejar los inputs y outputs del programa.
+*/
 int main(){
     StringManagment manager;
 
+    // Definimos variables y procesamos la entrada del alfabeto
     string abc, arg; 
     cout << "Alphabet: ";
     cin >> abc; 
@@ -17,17 +22,20 @@ int main(){
         return 0;
     }
 
+    // Procesamos la entrada de la expresión regular
     cout << "Regex: ";
     cin >> arg;
     arg = manager.polish(arg);
-    cout << arg << endl;
 
+    // Obtenemos el alfabeto para construir los autómatas
     set<char> alph = manager.getAlph();
     Graph gr(alph, arg);
 
+    // Construimos los autómatas, verificando si hay errores
     if(!gr.buildGraphs())
         return 0;
     
+    // Imprimimos los autómatas construidos
     cout << "\nNFA: ";
     gr.bfs();
 
